b5bcd57750ec37f1ece436d7facffbe2a135556c
leifnesse <leifenesse@gmail.com>
2018-03-02 04:02:47 -0600

    generated question controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302095719) do
+ActiveRecord::Schema.define(version: 20180302100217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20180302095719) do
     t.string   "photo"
     t.integer  "points"
     t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
