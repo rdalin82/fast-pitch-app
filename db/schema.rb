9dd8ef12d47c1bcf4f6bcd58f74e6e707d3d7491
leifnesse <leifenesse@gmail.com>
2018-03-02 04:32:10 -0600

    generated user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302100217) do
+ActiveRecord::Schema.define(version: 20180302103039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "presenter_questions", force: :cascade do |t|
+    t.integer  "presenter_id"
+    t.integer  "question_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "presenters", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +35,14 @@ ActiveRecord::Schema.define(version: 20180302100217) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
