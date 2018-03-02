468e4de6dbf94774c7b1b02f64269562266058eb
leifnesse <leifenesse@gmail.com>
2018-03-02 04:49:09 -0600

    generated score model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'pages#home'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/presenters' => 'presenters#index'
+  get '/presenters/new' => 'presenters#new'
+  post '/presenters' => 'presenters#create'
+  get '/presenters/:id' => 'presenters#show'
+  get '/presenters/:id/edit' => 'presenters#edit'
+  patch '/presenters/:id' => 'presenters#update'
+  delete '/presenters/:id' => 'presenters#destroy'
+
+  get '/questions' => 'questions#index'
+  get '/questions/new' => 'questions#new'
+  post '/questions' => 'questions#create'
+  get '/questions/:id' => 'questions#show'
+  get '/questions/:id/edit' => 'questions#edit'
+  patch '/questions/:id' => 'questions#update'
+  delete '/questions/:id' => 'questions#destroy'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302103039) do
+ActiveRecord::Schema.define(version: 20180302104716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20180302103039) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer  "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
