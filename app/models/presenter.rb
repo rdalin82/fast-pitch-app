165bb49bf1776a29c547c0529b28148725e58cc9
leifnesse <leifenesse@gmail.com>
2018-03-03 18:24:03 -0600

    built out presenters index and seed file for fake presenters

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -1,4 +1,4 @@
 class Presenter < ApplicationRecord
-  has_many :questions, through::question_presenter
+  has_many :questions, through: :question_presenter
   has_many :comments
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,4 @@
 class Question < ApplicationRecord
   has_many :presenters, through: :question_presenter
-  belongs_to :users
   has_many :scores
 end
