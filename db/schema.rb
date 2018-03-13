d774b18fcece67ac7fe9b83ca57d65afbcba202d
leifnesse <leifenesse@gmail.com>
2018-03-12 21:36:07 -0500

    removed unused columns in presenters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312015159) do
+ActiveRecord::Schema.define(version: 20180313023116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,6 @@ ActiveRecord::Schema.define(version: 20180312015159) do
 
   create_table "presenters", force: :cascade do |t|
     t.string   "name"
-    t.string   "photo"
-    t.integer  "points"
-    t.text     "comment"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "ranked",     default: false
